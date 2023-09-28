0d7877eea82b7a412147dbb6ce6fddd7a61e6d2c
汪志鹏 <13476859295@163.com>
2023-09-28 16:32:42 +0800

    achieved image change hover in achievement component

--- client/src/components/AchievementWall.jsx
+++ client/src/components/AchievementWall.jsx
@@ -12,7 +12,6 @@ const Container = styled.div`
     overflow: hidden;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
     align-items: center;
     padding: 10px;
     box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2);
@@ -20,12 +19,12 @@ const Container = styled.div`
 
 const TextStyle = styled.div`
     background-image: linear-gradient(90deg,#2e4f8b 0%,#3e589d 35%,#8f619b 60%,#d0718f 70%);
+    height:auto;
     -webkit-background-clip: text;
     background-clip: text;
     color: transparent;
     font-size: 36px;
     font-weight: bold;
-    flex-grow: 1;
     margin-bottom: 2vh;
     margin-top: 2vh;
 `;
@@ -35,13 +34,15 @@ function AchievementWall() {
   return (
     <Container>
       <TextStyle>AchievementWall</TextStyle>
-      {achievementList.map((achievement, index) => (
-        <Grid item xs={6} md={4} key={index}>
-          <Achievement 
-            achievement = {achievement}
-          />
-        </Grid>
-      ))}
+        <Grid container spacing={2}>
+        {achievementList.map((achievement, index) => (
+          <Grid item xs={6} md={4} key={index}>
+            <Achievement 
+              achievement = {achievement}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   )
 }
