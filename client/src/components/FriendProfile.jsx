5dba2014be54d63eadc4c30ae11cb73d1123df8c
汪志鹏 <13476859295@163.com>
2023-09-29 10:34:11 +0800

    add something

--- client/src/components/FriendProfile.jsx
+++ client/src/components/FriendProfile.jsx
@@ -10,18 +10,35 @@ const Container = styled.div`
     border-radius: 20px;
     overflow: hidden;
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
     padding: 10px;
     box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2);
 `
+
+const NameContainer = styled.div`
+  flex:1;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: lightblue;
+`
+
 function FriendProfile(props) {
   const { name, title, status } = props
   
   return (
     <Container>
-        {name}
+      <div style={{width:'100%', display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <NameContainer>{name}</NameContainer>
+        <div style={{flex:1, flexDirection:'column'}}>
+          <div>{name}</div>
+          <div>{title}</div>
+        </div>
+        <div style={{flex:1}}>
+          {status}
+        </div>
+      </div>
     </Container>
   )
 }
